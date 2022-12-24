1afff4fc18bc8b2a900c5534da9d6a3982ac718d
Stella <sarahprog667@gmail.com>
2022-12-24 13:01:42 +0800

    utf-8 __init__

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import re
 from setuptools import setup
 
 version_re = re.compile(r"^__version__ *= *('|\")((?:[0-9]+\.)*[0-9]+(?:\.?([a-z]+)(?:\.?[0-9])?)?)\1$", re.MULTILINE)
-with open(r'starlight/__init__.py', 'r') as r:
+with open(r'starlight/__init__.py', 'r', encoding="utf8") as r:
     ver = version_re.search(r.read())
     version = ver.group(2)
 
