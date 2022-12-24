cc6144efcce6e16b01c775531b2725f4fde28c63
Stella <sarahprog667@gmail.com>
2022-12-24 12:58:50 +0800

    update versioning

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
+import re
+
 from setuptools import setup
 
-version = '0.0.1'
+version_re = re.compile(r"^__version__ *= *('|\")((?:[0-9]+\.)*[0-9]+(?:\.?([a-z]+)(?:\.?[0-9])?)?)\1$")
+with open(r'starlight/__init__.py', 'r') as r:
+    ver = version_re.search(r.read())
+    version = ver.group(2)
+
 packages = [
     "starlight",
     "starlight.help_command",
@@ -15,32 +21,33 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 
-setup(name='starlight-dpy',
-      author='InterStella0',
-      url='https://github.com/InterStella0/starlight-dpy',
-      project_urls={
-        "Issue tracker": "https://github.com/InterStella0/starlight-dpy/issues",
-      },
-      version=version,
-      packages=packages,
-      license='MIT',
-      description='A Utility library for discord.py',
-      include_package_data=True,
-      install_requires=requirements,
-      python_requires='>=3.8.0',
-      classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Internet',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Utilities',
-        'Typing :: Typed',
-      ]
+setup(
+    name='starlight-dpy',
+    author='InterStella0',
+    url='https://github.com/InterStella0/starlight-dpy',
+    project_urls={
+      "Issue tracker": "https://github.com/InterStella0/starlight-dpy/issues",
+    },
+    version=version,
+    packages=packages,
+    license='MIT',
+    description='A Utility library for discord.py',
+    include_package_data=True,
+    install_requires=requirements,
+    python_requires='>=3.8.0',
+    classifiers=[
+      'Development Status :: 2 - Pre-Alpha',
+      'License :: OSI Approved :: MIT License',
+      'Intended Audience :: Developers',
+      'Natural Language :: English',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python :: 3.8',
+      'Programming Language :: Python :: 3.9',
+      'Programming Language :: Python :: 3.10',
+      'Topic :: Internet',
+      'Topic :: Software Development :: Libraries',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      'Topic :: Utilities',
+      'Typing :: Typed',
+    ]
 )
