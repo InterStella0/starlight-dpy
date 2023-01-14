e5c6633d127a06c6830613fa22df78cdf44dfa1b
Stella <sarahprog667@gmail.com>
2023-01-14 15:04:20 +0800

    version

--- starlight/__init__.py
+++ starlight/__init__.py
@@ -13,7 +13,7 @@ __title__ = 'starlight-dpy'
 __author__ = 'InterStella0'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2022-present InterStella0'
-__version__ = '0.0.1a29'
+__version__ = '0.0.1a30'
 
 
 class VersionInfo(NamedTuple):
@@ -24,4 +24,4 @@ class VersionInfo(NamedTuple):
     serial: int
 
 
-version_info: VersionInfo = VersionInfo(major=0, minor=0, micro=1, level='a', serial=29)
+version_info: VersionInfo = VersionInfo(major=0, minor=0, micro=1, level='a', serial=30)
