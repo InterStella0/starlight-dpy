ce7916a8e5285789800a26a4c9adb16f5e867334
Stella <sarahprog667@gmail.com>
2023-03-08 00:11:16 +0800

    packages issue

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 import re
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 version_re = re.compile(r"^__version__ *= *('|\")((?:[0-9]+\.)*[0-9]+(?:\.?([0-z]+)(?:\.?[0-9])?)?)\1$", re.MULTILINE)
 with open(r'starlight/__init__.py') as r:
     ver = version_re.search(r.read())
     version = ver.group(2)
 
+packages = [
+    "starlight",
+    "starlight.star_commands",
+    "starlight.star_commands.help_command",
+    "starlight.star_commands.views",
+    "starlight.utils",
+]
 
 with open('README.md', encoding="utf8") as f:
     readme = f.read()
@@ -23,7 +30,7 @@ setup(
       "Issue tracker": "https://github.com/InterStella0/starlight-dpy/issues",
     },
     version=version,
-    packages=find_packages(),
+    packages=packages,
     license='MIT',
     description='A Utility library for discord.py',
     include_package_data=True,
