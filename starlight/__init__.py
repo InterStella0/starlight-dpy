dc59a389c457158b47a02c6191035ea864d80e7a
Stella <sarahprog667@gmail.com>
2023-03-08 00:02:40 +0800

    added separators

--- starlight/__init__.py
+++ starlight/__init__.py
@@ -13,7 +13,7 @@ __title__ = 'starlight-dpy'
 __author__ = 'InterStella0'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2022-present InterStella0'
-__version__ = '0.0.1a32'
+__version__ = '0.0.1a33'
 
 
 class VersionInfo(NamedTuple):
